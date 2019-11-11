a6db7330f0f0bc762bba81c0f7490a79aebb1aa2
Tomasz Lisowski <t.p.lisowski@gmail.com>
2019-11-11 15:28:48 +0100

    Tune parameters
    These parameters should result in the fastest robot while remaining within the safe ranges set by the physcom library

--- constants.h
+++ constants.h
@@ -1,18 +1,18 @@
 // Motor speeds
-static const float MOTOR_MAX = 0.3;
+static const float MOTOR_MAX = 0.71;
 static const float MOTOR_MIN = 0.0;
 
 // PID tuning parameters
-static const float KP = 1; // 1
-static const float KI = 0; // 0.5
-static const float KD = 3; // 3.5
+static const float KP = 1;
+static const float KI = 0;
+static const float KD = 80;
 
 // Optosensor parameters
-static const uint8_t SENSOR_FIRST_INDEX = 1;
-static const uint8_t SENSOR_COUNT = 3;
-static const uint8_t NOISE_THRESHOLD = 50;
-static const uint8_t LINE_DETECTION_THRESHOLD = 200;
-static const uint16_t ABOVE_LINE_THRESHOLD = 2 * LINE_DETECTION_THRESHOLD;
+static const uint8_t SENSOR_FIRST_INDEX = 0;
+static const uint8_t SENSOR_COUNT = 5;
+static const uint16_t NOISE_THRESHOLD = 60;
+static const uint16_t LINE_DETECTION_THRESHOLD = 200;
+static const uint16_t ABOVE_LINE_THRESHOLD = 850;
 
 static const uint16_t RANGE_MIN = 0;
 static const uint16_t RANGE_MAX = (SENSOR_COUNT - 1) * 1000;
